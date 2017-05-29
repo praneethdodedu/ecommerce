632473beefb9efd88ff3310f00f2735d74c5c7d5
Vedran Karacic <karacicvedran@gmail.com>
2017-05-29 16:53:20 +0200

    Fix import order

--- ecommerce/courses/tests/test_models.py
+++ ecommerce/courses/tests/test_models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.utils.timezone import now, timedelta
 from freezegun import freeze_time
 from oscar.core.loading import get_model
-from oscar.test.factories import create_order, PartnerFactory
+from oscar.test.factories import PartnerFactory, create_order
 from oscar.test.newfactories import BasketFactory
 
 from ecommerce.core.constants import ENROLLMENT_CODE_PRODUCT_CLASS_NAME, ENROLLMENT_CODE_SWITCH
